886576c529af413756a515ea4fc97724fe9be120
huynqhe186195 <166681804+huynqhe186195@users.noreply.github.com>
2026-01-08 14:39:17 +0700

    feat: add view home

---
+++ src/main/java/com/generatorproject/controller/web/HomeController.java
@@ -0,0 +1,48 @@
+package com.generatorproject.controller.web;
+
+import com.generatorproject.model.Users;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/home"})
+public class HomeController extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1. Giả lập lấy dữ liệu từ Database
+        String dashboardTitle = "Hệ Thống Quản Lý Máy Phát Điện (Gen-CMS)";
+        int totalGenerators = 25; // Tổng số máy
+        int runningGenerators = 18; // Đang chạy
+        int maintenanceGenerators = 5; // Bảo trì
+        int errorGenerators = 2; // Đang lỗi
+
+        Users user = new Users();
+        user.setFullName("Vũ Ngọc Trang");
+        req.setAttribute("userInfo", user);
+
+        // 2. Đẩy dữ liệu sang trang JSP để hiển thị
+        req.setAttribute("title", dashboardTitle);
+        req.setAttribute("total", totalGenerators);
+        req.setAttribute("running", runningGenerators);
+        req.setAttribute("maintenance", maintenanceGenerators);
+        req.setAttribute("error", errorGenerators);
+
+        // 3. Chuyển hướng về giao diện
+        // Lưu ý: Đường dẫn này phải đúng với nơi bạn đặt file jsp
+        RequestDispatcher rd = req.getRequestDispatcher("/views/Home/Home.jsp");
+        rd.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
---
+++ src/main/webapp/views/Admin/admin.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Admin</title>
+</head>
+<body>
+<h2>Hello JSP - IntelliJ</h2>
+<h2>Hello JSP - Tôi là Admin</h2>
+</body>
+</html>
